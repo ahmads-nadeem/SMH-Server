8c4acc1c8664a5dd0000f7425dd2632480e46d6a
ahmads-nadeem <asyouknowme119@gmial.com>
2026-06-25 14:11:18 +0500

    MVC Structure Implimentation

--- controller/medicineController.js
+++ controller/medicineController.js
@@ -1,5 +1,6 @@
 const db = require('../utils/databaseutil');
-const { storeMedicines } = require('../model/medicineModel');
+const { storeMedicines, checkMedicin, insertStoreMedicin, refillMedicin, addNewPurchase } = require('../model/medicineModel');
+
 
 exports.purchaseAssets = async (req, res, next) => {
     let {
@@ -26,29 +27,24 @@ exports.purchaseAssets = async (req, res, next) => {
     const pQty = parseInt(purchaseQuantity, 10) || 0;
     const packQty = parseInt(packQuantity, 10) || 0;
 
-    let [rows] = await db.execute('SELECT * FROM storemedicines WHERE productName = ? AND power = ? AND category = ?', [name.toLowerCase(), power.toLowerCase(), category.toLowerCase()]);
-    if(rows.length == 0){
-        console.log('medicin not present');
-        let querymed = 'INSERT INTO storemedicines (productName, formula, category, boxprice, power, nopiec, adddate, sellprice, expirydate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'
-        let [result] = await db.execute(querymed, [name.toLowerCase(), formula.toLowerCase(), category.toLowerCase(), pPrice, power.toLowerCase(), packQty, receivingDate, sPrice, expiryDate]);
+    const rows = db.checkMedicin(name, power, category);
+    let medResult;
+    if (rows.length == 0) {
+        medResult = insertStoreMedicin(name, formula, category, pPrice, power, packQty, receivingDate, sPrice, expiryDate);
     }
-    else{
-        console.log('medicine is present');
-        let updatequery = 'UPDATE storemedicines SET boxprice = ?, nopiec = ?, sellprice = ? WHERE productId = ?';
-        let [row] = await db.execute(updatequery, [pPrice, packQty, sPrice, rows[0].productId]);
-    }    
-
-    let query = 'INSERT INTO assetspurchase (medname, formula, category, power, purchaserate, purchaseprice,  sellprice, purchasequantity, packquantity, supplier, receivingdate, expirydate, paymentstatus) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    const [result] = await db.execute(query, [name, formula, category, power, pRate, pPrice, sPrice, pQty, packQty, supplier, receivingDate, expiryDate, paymentStatus]);
+    else {
+        medResult = refillMedicin(pPrice, packQty, sPrice, rows[0].productId)
+    }
+    let newPurchase = addNewPurchase(name, formula, category, power, pRate, pPrice, sPrice, pQty, packQty, supplier, receivingDate, expiryDate, paymentStatus)
     res.status(201).json({
-        message: "Asset successfully purchased!",
-        insertedId: result.insertId
+        message: (rows.length == 0? "New Medicin Inserted into DataBase and Purchase is Created": "Medicin is refilled and Purchase is Created"),
+        insertedId: newPurchase.insertId
     });
 }
-exports.allMedicines =  async (req, res, next) => {
-    console.log('GET request is received!');
+
+exports.allMedicines = async (req, res, next) => {
     const medicines = await storeMedicines();
-    res.json(medicines[0])
+    res.json(medicines[0]);
 }
 
 
--- model/medicineModel.js
+++ model/medicineModel.js
@@ -1,10 +1,39 @@
 const db = require('../utils/databaseutil');
+const checkMedicin = async (name, power, category) => {
+    let [rows] = await db.execute('SELECT * FROM storemedicines WHERE productName = ? AND power = ? AND category = ?', [name.toLowerCase(), power.toLowerCase(), category.toLowerCase()]);
+    return rows
+}
+const insertStoreMedicin = async (name, formula, category, pPrice, power, packQty, receivingDate, sPrice, expiryDate) => {
+    try{
+        let querymed = 'INSERT INTO storemedicines (productName, formula, category, boxprice, power, nopiec, adddate, sellprice, expirydate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        let [result] = await db.execute(querymed, [name.toLowerCase(), formula.toLowerCase(), category.toLowerCase(), pPrice, power.toLowerCase(), packQty, receivingDate, sPrice, expiryDate]);
+        return result
+    }
+    catch(error){
+        throw error
+    }
+}
+
+const refillMedicin = async(pPrice, packQty, sPrice, productId) => {
+    try{
+        let updatequery = 'UPDATE storemedicines SET boxprice = ?, nopiec = ?, sellprice = ? WHERE productId = ?';
+        let [row] = await db.execute(updatequery, [pPrice, packQty, sPrice, productId]);
+        return row
+    }
+    catch(error){
+        throw error
+    }
+}
+
+const addNewPurchase = async (name, formula, category, power, pRate, pPrice, sPrice, pQty, packQty, supplier, receivingDate, expiryDate, paymentStatus) => {
+    let query = 'INSERT INTO assetspurchase (medname, formula, category, power, purchaserate, purchaseprice,  sellprice, purchasequantity, packquantity, supplier, receivingdate, expirydate, paymentstatus) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const [result] = await db.execute(query, [name, formula, category, power, pRate, pPrice, sPrice, pQty, packQty, supplier, receivingDate, expiryDate, paymentStatus]);
+
+}
 
 const storeMedicines = async() => {
     let medicines = await db.execute('SELECT * FROM storemedicines');
     return medicines;
 }
 
-
-
-module.exports = { storeMedicines };
+module.exports = { storeMedicines, checkMedicin, insertStoreMedicin, refillMedicin, addNewPurchase };
